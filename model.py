53fc5abf0892806d2c043ff4f34a6f4640d26b85
friendflower94 <52149970+friendflower94@users.noreply.github.com>
2021-10-03 17:29:24 +0900

    Update model.py

--- model.py
+++ model.py
@@ -1,62 +1,30 @@
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
-import time
 
-def train(model, device, loader, optimizer, epoch):
-    model.train()
-    data_size = len(loader)
-    
-    start = time.time()
-    for batch_idx, (X, y) in enumerate(loader):
-        X = X.to(device)
-        y =  y.to(device)
-        optimizer.zero_grad()
-        out = model(X)
-        loss = F.nll_loss(out, y)
-        loss.backward()
-        optimizer.step()
+##### AdaIN #####
+def adaptive_instance_normalization(content, style):
+    assert(content.size()[:2]==style.size()[:2])
+    size = content.size()
 
-        print("\rTrain Epoch: {} [ {:0=5}/{:0=5} ({:.0f}%)]\t Loss: {:.4f}".format(epoch,
-            batch_idx+1, data_size, (batch_idx + 1) * 100. / data_size, loss.item()
-        ), end="")
+    mu_content, mu_style = torch.mean(content, axis=2), torch.mean(style, axis=2)
+    sigma_content, sigma_style = torch.std(content, axis=2), torch.std(style, axis=2)
 
-    end = time.time()
+    normalized = (content - mu_content.expand(size)) / sigma_content.expand(size)
 
-    print("\tThis took {:.3f} seconds".format(end-start), end="")
+    return normalized * sigma_style.expand(size) + mu_style.expand(size)
 
-def test(model, device, loader):
-    model.eval()
 
-    val_loss = 0
-    true = 0
-    data_size = len(loader) * loader.batch_size
-
-    with torch.no_grad():
-        for X, y in loader:
-            X, y = X.to(device), y.to(device)
-            out = model(X)
-
-            val_loss += F.nll_loss(out, y, reduction="sum").item()
-            y_pred = out.argmax(dim=1, keepdim=True)
-            true += y_pred.eq(y.view_as(y_pred)).sum().item()
-
-    val_loss /= data_size
-    true /= data_size
-
-    print("\nTest set: Average loss: {:.4f}, Accuracy: {:.0f}%".format(val_loss, true*100))
-    
-    return val_loss
-
+##### gene discriminator #####
 class Discriminator(nn.Module):
     def __init__(self, input_dim, output_dim):
         super(Discriminator, self).__init__()
-        
+
         self.input_dim = input_dim
         self.output_dim = output_dim
 
         self.conv_layers = nn.Sequential(
-            self.conv_relu(4, 128, 3, 1),
+            self.conv_relu(4, 128, 4, 1),
             self.conv_relu(128, 128, 3, 1),
             self.conv_relu(128, 256, 3, 1),
             self.conv_relu(256, 256, 3, 1),
@@ -95,16 +63,41 @@ class Discriminator(nn.Module):
         x = self.conv_layers[:layer](x)
 
         return x
-
+    
     def get_gap(self,x):
         x = self.conv_layers(x)
         x = torch.mean(x, dim=2)
 
-        return x
+        return x    
 
     def get_style(self, x, layer):
         x = self.conv_layers[:layer](x)
         x = torch.squeeze(x)
         gram = torch.mm(x, torch.t(x))
-        
+
         return gram
+
+##### decoder #####
+class Decoder(nn.Module):
+    def __init__(self, input_dim, output_dim):
+        super(Decoder, self).__init__()
+
+        self.input_dim = input_dim
+        self.output_dim = output_dim
+
+        self.conv_layers = nn.Sequential(
+            self.conv_relu(64, 64, 3, 1),
+            self.conv_relu(64, 32, 3, 1),
+            self.conv_relu(32, 32, 3, 1),
+            self.conv_relu(32, 4, 3, 1)
+        )
+
+    def conv_relu(self, input_dim, output_dim, kernel_size, padding):
+        return nn.Sequential(
+            nn.Conv1d(input_dim, output_dim, kernel_size=kernel_size, padding=padding),
+            nn.ReLU(),
+            nn.Upsample(2)
+        )
+
+    def forward(self, x):
+        return F.softmax(self.conv_layers(x), axis=1)
