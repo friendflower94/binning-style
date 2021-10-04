8d139d990850eb6294560a38bbdd0dc5c4277988
friendflower94 <52149970+friendflower94@users.noreply.github.com>
2021-10-04 12:51:24 +0900

    Update main.py

--- main.py
+++ main.py
@@ -20,7 +20,6 @@ if __name__ == "__main__":
     device = "cuda" if torch.cuda.is_available() else "cpu"
     device = torch.device(device)
     
-    # abc
     # read trainingdata
     if args.verbose > 1:
         print("Reading training data...")
