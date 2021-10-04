d93c0ca29db1c9200a3d293bebb5b596927ab4e2
friendflower94 <52149970+friendflower94@users.noreply.github.com>
2021-10-04 15:55:50 +0900

    Update loader.py

--- loader.py
+++ loader.py
@@ -65,6 +65,74 @@ def read_contig(dir):
     return seqs, labels
 
 class DataLoader():
+    def __init__(self, length, n_batches, batch_size=64, how="random"):
+        self.batch_size = batch_size
+        self.how = how
+        self.n_batches = n_batches
+        self._i = 0
+        self.length = length
+        
+    def __call__(self, species, seqs, labels):
+        self.species, self.seqs, self.labels = species, seqs, labels
+        self.lens = np.array([seq.shape[2] for seq in seqs])
+        
+    def __iter__(self):
+        return self
+
+    def __next__(self):
+        if self._i >= self.n_batches:
+            self._i = 0
+            raise StopIteration()
+
+        self._i += 1
+
+        X_batch, y_batch = self.get_batch()
+
+        return X_batch, y_batch
+
+    def __len__(self):
+        return self.n_batches
+
+    def get_batch(self):
+        if self.how=="random":
+            # select species
+            X_batch = []
+            indexes = np.random.randint(len(self.species), size=self.batch_size)
+            lens = self.lens[indexes]
+
+            for index, length in zip(indexes, lens):
+                start = np.random.randint(length - self.length)
+                X_batch.append(self.seqs[index][:,:,start:start+self.length])
+
+            return torch.cat(X_batch), self.labels[indexes]
+
+    def train_test_split(self, test_size=0.2):
+        train_loader = DataLoader(self.length, self.n_batches, self.batch_size, self.how)
+        test_loader = DataLoader(self.length, 100, self.batch_size, self.how)
+
+        bounds = np.array([int(x * (1-test_size)) for x in self.lens])
+        train_seq = [seq[:,:,:bound] for seq, bound in zip(self.seqs, bounds)]
+        test_seq = [seq[:,:,bound:] for seq, bound in zip(self.seqs, bounds)]
+
+        train_loader(self.species, train_seq, self.labels)
+        test_loader(self.species, test_seq, self.labels)
+
+        return train_loader, test_loader
+    
+    def sample(self, size, index):
+        print("Sampling {}".format(self.species[index]))
+        
+        X_batch = []
+        indexes = (np.zeros(size) + index).astype(np.int64)
+        lens = self.lens[indexes]
+
+        for index, length in zip(indexes, lens):
+            start = np.random.randint(length - self.length)
+            X_batch.append(self.seqs[index][:,:,start:start+self.length])
+
+        return torch.cat(X_batch)
+"""
+class DataLoader():
     def __init__(self, batch_size, how="random", length=1024, is_train=True, use_all=False, test_size=0.3, n_batch=1000):
         self.is_train = is_train
         self.batch_size = batch_size
@@ -119,3 +187,4 @@ class DataLoader():
     def get_one(self, ind):
         start = np.random.randint(self.lens[ind]-self.length)
         return torch.tensor(self.seqs[ind][start:start+self.length].T).unsqueeze(0)
+"""
